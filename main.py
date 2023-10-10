b26920e0edcee48517f5f17d077e59b03de8efd7
JosueLessie <joslessie@hotmail.com>
2023-10-10 22:02:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Create two sets, set1 and set2, with some common and unique elements.
+# Find the intersection of set1 and set2.
+
+set1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+set2 = {4, 5, 6, 7, 8}
+intersection = set1.intersection(set2)
+print(intersection)
